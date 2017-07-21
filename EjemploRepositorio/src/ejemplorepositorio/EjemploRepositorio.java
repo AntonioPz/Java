8db2f2db5a4e837186b04b47b4a9b9d8183c4a5c
Antonio <Antonio@DESKTOP-O-0>
2017-07-20 22:46:48 -0500

    Actualización 2

--- EjemploRepositorio/src/ejemplorepositorio/EjemploRepositorio.java
+++ EjemploRepositorio/src/ejemplorepositorio/EjemploRepositorio.java
@@ -16,6 +16,7 @@ public class EjemploRepositorio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
